20b7a50737ce62214d457d1fc6475443e88a9e6e
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2023-04-16 05:17:48 -0600

    model dimensions updated, training/val/test code complete, plotting data

--- models/CNN_SE_model.py
+++ models/CNN_SE_model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SEBlock(nn.Module):
-    def __init__(self, in_channels, reduction=16):
+    def __init__(self, in_channels, reduction=4):
         super(SEBlock, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Linear(in_channels, in_channels // reduction)
@@ -21,8 +21,8 @@ class SEBlock(nn.Module):
 
 
 
-class CNN_SE(nn.module):
-    def __init__(self):
+class CNN_SE(nn.Module):
+    def __init__(self, num_of_classes):
         super(CNN_SE, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.seblock1 = SEBlock(16)
@@ -33,9 +33,9 @@ class CNN_SE(nn.module):
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.seblock3 = SEBlock(64)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 4 * 4, 256)
+        self.fc1 = nn.Linear(64 * 28 * 28, 256)
         self.bn1 = nn.BatchNorm1d(256)
-        self.fc2 = nn.Linear(256, 10)
+        self.fc2 = nn.Linear(256, num_of_classes)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -47,7 +47,7 @@ class CNN_SE(nn.module):
         x = F.relu(self.conv3(x))
         x = self.seblock3(x)
         x = self.pool3(x)
-        x = x.view(-1, 64 * 4 * 4)
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = F.relu(self.bn1(x))
         x = self.fc2(x)
--- models/CNN_model.py
+++ models/CNN_model.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN(nn.module):
-    def __init__(self):
+class CNN(nn.Module):
+    def __init__(self, num_of_classes):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -10,9 +10,9 @@ class CNN(nn.module):
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 4 * 4, 256)
+        self.fc1 = nn.Linear(64 * 28 * 28, 256)
         self.bn1 = nn.BatchNorm1d(256)
-        self.fc2 = nn.Linear(256, 10)
+        self.fc2 = nn.Linear(256, num_of_classes)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -21,7 +21,7 @@ class CNN(nn.module):
         x = self.pool2(x)
         x = F.relu(self.conv3(x))
         x = self.pool3(x)
-        x = x.view(-1, 64 * 4 * 4)
+        x = x.view(x.size(0),-1)
         x = self.fc1(x)
         x = F.relu(self.bn1(x))
         x = self.fc2(x)
