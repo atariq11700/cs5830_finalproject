d9be87fcb83c67ae8da93865613bfef72c546cf2
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2023-04-17 00:17:13 -0600

    kernel sizes, data transformations, learning rate

--- models/CNN_model.py
+++ models/CNN_model.py
@@ -4,13 +4,13 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self, num_of_classes):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=12, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=8, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 28 * 28, 256)
+        self.fc1 = nn.Linear(64 * 25 * 25, 256)
         self.bn1 = nn.BatchNorm1d(256)
         self.fc2 = nn.Linear(256, num_of_classes)
     
@@ -23,6 +23,6 @@ class CNN(nn.Module):
         x = self.pool3(x)
         x = x.view(x.size(0),-1)
         x = self.fc1(x)
-        x = F.relu(self.bn1(x))
+        x = F.dropout(F.relu(self.bn1(x)))
         x = self.fc2(x)
         return x
