6dcc0e36e1ca02f8e556a72f79f13cd14d392fcc
Kevin Vulcano <kevin.vulcano@aggiemail.usu.edu>
2023-04-21 14:26:05 -0600

    arch-1

--- models/CNN_model.py
+++ models/CNN_model.py
@@ -4,15 +4,15 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self, num_of_classes):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=12, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=12, stride=4, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=8, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=8, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=1, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 25 * 25, 256)
-        self.bn1 = nn.BatchNorm1d(256)
-        self.fc2 = nn.Linear(256, num_of_classes)
+        self.fc1 = nn.Linear(256 * 5 * 5, 320)
+        self.bn1 = nn.BatchNorm1d(320)
+        self.fc2 = nn.Linear(320, num_of_classes)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -25,4 +25,4 @@ class CNN(nn.Module):
         x = self.fc1(x)
         x = F.dropout(F.relu(self.bn1(x)))
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
